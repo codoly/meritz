f4b7ede554a32e028b5b3201434865ff311b001c
codoly@nate.com <xoaud0020>
2019-12-09 09:21:24 +0900

    list 수정

--- asset/js/default.js
+++ asset/js/default.js
@@ -1,30 +1,30 @@
-$(function(){
-    //셀렉트 디자인변경
-    $(".selectBox select").niceSelect();
+$(function () {
+  //셀렉트 디자인변경
+  $(".selectBox select").niceSelect();
 
-    //searchArea foucs event
-    $(".searchArea").on("focusin", function(){
-        $(this).addClass("focus");
-    })
-    $(".searchArea").on("focusout", function(){
-        $(this).removeClass("focus");
-    })
-    //ie9에서 placeholder처리
-    $('input, textarea').placeholder();
-    //input focusOut 처리
-    $(document).on("focusout",".inputSet input", function(){
-        var thisValue = $(this).attr("data-value")
-        if(thisValue != $(this).val()){
-            $(this).parent().addClass("change")
-        }else{
-            $(this).parent().removeClass("change")
-        }
-    })
+  //searchArea foucs event
+  $(".searchArea").on("focusin", function () {
+    $(this).addClass("focus");
+  })
+  $(".searchArea").on("focusout", function () {
+    $(this).removeClass("focus");
+  })
+  //ie9에서 placeholder처리
+  $('input, textarea').placeholder();
+  //input focusOut 처리
+  $(document).on("focusout", ".inputSet input", function () {
+    var thisValue = $(this).attr("data-value")
+    if (thisValue != $(this).val()) {
+      $(this).parent().addClass("change")
+    } else {
+      $(this).parent().removeClass("change")
+    }
+  })
 
-    //팝업테스트함수
-    $(".logo").click(function(){
-        var popUrl =window.location;
-        var popOption = "width=1300px, height=920px, resizable=no, location=no, top=0px, left=0px, scrollable=no;"
-        window.open(popUrl,"index",popOption);
-    });
+  //팝업테스트함수
+  $(".logo").click(function () {
+    var popUrl = window.location;
+    var popOption = "width=1300px, height=920px, resizable=no, location=no, top=0px, left=0px, scrollable=no;"
+    window.open(popUrl, "index", popOption);
+  });
 })
