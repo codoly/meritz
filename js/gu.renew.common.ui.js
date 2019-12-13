7a264f893f5d46db9a5c790bc82ae4622cf25867
codoly@nate.com <xoaud0020>
2019-12-13 10:54:40 +0900

    전체수정

--- js/gu.renew.common.ui.js
+++ js/gu.renew.common.ui.js
@@ -1,18 +1,18 @@
 $(function () {
   //셀렉트 디자인변경
-  $(".selectBox select").niceSelect();
+  $(".select_box select").niceSelect();
 
   //searchArea foucs event
-  $(".searchArea").on("focusin", function () {
+  $(".search_area").on("focusin", function () {
     $(this).addClass("focus");
   })
-  $(".searchArea").on("focusout", function () {
+  $(".search_area").on("focusout", function () {
     $(this).removeClass("focus");
   })
   //ie9에서 placeholder처리
   $('input, textarea').placeholder();
   //input focusOut 처리
-  $(document).on("focusout", ".inputSet input", function () {
+  $(document).on("focusout", ".input_set input", function () {
     var thisValue = $(this).attr("data-value")
     if (thisValue != $(this).val()) {
       $(this).parent().addClass("change")
@@ -29,24 +29,24 @@ $(function () {
   });
 
   //layout sub 좌측 하단 멤버 리스트
-  $(document).on("click", ".memberList .list .item .name", function () {
+  $(document).on("click", ".member_list .list .item .name", function () {
     var target = $(this).closest(".item");
     var targetList = $(this).closest("ul").find("> li");
-    if (!target.hasClass("on")){
+    if (!target.hasClass("on")) {
       targetList.find(".item").removeClass("on");
       targetList.find(".item .info").slideUp();
       target.addClass("on");
       target.find(".info").slideDown();
-    }else{
+    } else {
       target.removeClass("on");
       target.find(".info").slideUp();
     }
   })
 })
 
-function counterStart(){
+function counterStart() {
   $('.count').each(function () {
-    $(this).prop('Counter',0).animate({
+    $(this).prop('Counter', 0).animate({
       Counter: $(this).text()
     }, {
       duration: 1000,
@@ -59,26 +59,29 @@ function counterStart(){
 }
 
 //layerpopup
-function layerPopBoxOpen(target){
-  $("#"+target).show();
-  setTimeout(function(){
-    $("#"+target).addClass("open");
-  },10)
+function layerPopBoxOpen(target) {
+  $("#" + target).show();
+  setTimeout(function () {
+    $("#" + target).addClass("open");
+  }, 10)
 
 }
-function layerPopBoxClose(target){
-  $("#"+target).hide().removeClass("open");;
+
+function layerPopBoxClose(target) {
+  $("#" + target).hide().removeClass("open");
+  ;
 }
+
 //탑버튼 유무
-function btnTop(obj){
-  var btnTop = $(".btnTop");
+function btnTop(obj) {
+  var btnTop = $(".btn_top");
   console.log($(obj).height(), $(obj).find(".mCSB_container").height())
-  if($(obj).height() < $(obj).find(".mCSB_container").height()){
+  if ($(obj).height() < $(obj).find(".mCSB_container").height()) {
     btnTop.show();
     btnTop.on("click", function () {
-      $(obj).mCustomScrollbar( "scrollTo","0")
+      $(obj).mCustomScrollbar("scrollTo", "0")
     })
-  }else{
+  } else {
     btnTop.hide();
   }
 }
