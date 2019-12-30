6d1b8960b0492b707dc4cfef793a9f80463caa14
tmkim <tmkim@alwaysinc.co.kr>
2019-12-30 14:15:57 +0900

    모바일 작업

--- mobile/resources/js/gu.renew.common.ui.js
+++ mobile/resources/js/gu.renew.common.ui.js
@@ -101,6 +101,24 @@ function layerPopBoxClose(target) {
 	$("#" + target).hide().removeClass("open");
 	;
 }
+//input cursor위치
+$.fn.setCursorPosition = function( pos )
+{
+
+	this.each( function( index, elem ) {
+		if( elem.setSelectionRange ) {
+			elem.setSelectionRange(pos, pos);
+		} else if( elem.createTextRange ) {
+			var range = elem.createTextRange();
+			range.collapse(true);
+			range.moveEnd('character', pos);
+			range.moveStart('character', pos);
+			range.select();
+		}
+	});
+
+	return this;
+};
 
 
 
